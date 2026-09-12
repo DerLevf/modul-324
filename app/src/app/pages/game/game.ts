e5c01996dbe37f88148f2883abe265f8b3cbe14d
yannick.buechler@bbzbl-it.ch <yannick.buechler@bbzbl-it.ch>
2026-09-12 13:40:22 +0200

    feat(wordle): add game page

---
+++ app/src/app/pages/game/game.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Layout } from '../../components/layout/layout';
+
+@Component({
+  imports: [Layout],
+  selector: 'app-game',
+  styleUrl: './game.css',
+  templateUrl: './game.html',
+})
+export class Game {}
