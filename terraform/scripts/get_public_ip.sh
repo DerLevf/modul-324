8157fe2c44d5371c6ea2496be6226bcf5f58e1a1
YannickBBZBL1 <yannick.buechler@bbzbl-it.ch>
2026-09-21 15:02:27 +0200

    chore: add infrastructure provisioning workflow #77 (#78)

---
+++ terraform/scripts/get_public_ip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# INFO: $1   : Beinhaltet der erste Parameter, der dem Script übergeben wird
+#       2>&1 : Sendet Errors nach /dev/null, damit das Script nicht beendet wird
+echo $(aws ec2 describe-instances --filter Name=tag:App,Values=speedl --query 'Reservations[*].Instances[?!contains(State.Name, `terminated`)].{Instance:PublicIpAddress}' --output text)
