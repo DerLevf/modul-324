442190967611694142f7babd7da9bda75401b163
lorisschaer-hub <loris.schaer@bbzbl-it.ch>
2026-09-21 15:55:26 +0200

    chore: add umlauts

--- app/src/app/pages/game/components/keyboard/keyboard.ts
+++ app/src/app/pages/game/components/keyboard/keyboard.ts
@@ -7,8 +7,8 @@ import { Component, output } from '@angular/core';
 })
 export class Keyboard {
   readonly rows = [
-    ['Q', 'W', 'E', 'R', 'T', 'Z', 'U', 'I', 'O', 'P'],
-    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    ['Q', 'W', 'E', 'R', 'T', 'Z', 'U', 'I', 'O', 'P', 'Ü'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ö', 'Ä'],
     ['Y', 'X', 'C', 'V', 'B', 'N', 'M'],
   ];
 
